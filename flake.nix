715ae15bf130fd8b1484c929d39ece81dcc5e33d
Jack Maloney <jack@maloney.ai>
2022-12-26 12:25:17 -0600

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
     description = "NixOS, nix-darwin, and home-manager configurations.";
 
     inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs-channels/nixos-unstable";
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
         darwin = {
             url = "github:lnl7/nix-darwin/master";
