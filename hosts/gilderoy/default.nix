dfb1c7299b9ecd275f510433d51b6c5b97466cc2
Jack Maloney <jack@maloney.ai>
2023-01-21 19:21:42 -0600

    updates

--- hosts/gilderoy/default.nix
+++ hosts/gilderoy/default.nix
@@ -10,6 +10,39 @@ in inputs.nixpkgs.lib.nixosSystem
     ../../shared/packages.nix
 
     inputs.home-manager.nixosModules.home-manager
+    ({ config, pkgs, lib, ... }:
+    let
+        username = "jack";
+    in
+    {
+        nixpkgs.config.allowUnfree = true;
+
+        networking.hostName = "gilderoy";
+
+        users.users.${username}.home = "/Users/${username}";
+        # nix.nixPath.nixpkgs = "${inputs.nixpkgs-unstable}";
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.extraSpecialArgs = { inherit inputs system; };
+        home-manager.users.${username} = {
+            imports = [
+                ../../home/default.nix
+                ../../home/git.nix
+                ../../home/starship.nix
+                inputs.nix-doom-emacs.hmModule
+            ];
+            home.stateVersion = "22.11";
+            programs.doom-emacs = {
+                enable = true;
+                doomPrivateDir = ../../doom.d;
+                emacsPackage = pkgs.emacs-nox;
+            };
+        };
+
+        # see: https://nix-community.github.io/home-manager/options.html#opt-programs.zsh.enableCompletion
+        environment.pathsToLink = [ "/share/zsh" ];
+    })
+
     ({ config, pkgs, lib, ... }: inputs.nixpkgs.lib.mkMerge {
       networking.hostName = "gilderoy";
     })
